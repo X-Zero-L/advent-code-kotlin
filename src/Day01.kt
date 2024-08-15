c3871e455781841c2df1906ccf4349fd80b5a5b4
GitHub Action <action@github.com>
2024-08-15 15:01:36 +0000

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
+import java.math.BigInteger
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun groupAndSumInput(input: List<String>): List<BigInteger> =
+        input.fold(mutableListOf(BigInteger.ZERO)) { acc, line ->
+            if (line.isBlank()) {
+                acc.add(BigInteger.ZERO)
+            } else {
+                acc[acc.lastIndex] += line.toBigInteger()
+            }
+            acc
+        }
+
+    fun part1(input: List<String>): BigInteger {
+        return groupAndSumInput(input).max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>, topn: Int = 3): BigInteger {
+        return groupAndSumInput(input).sortedDescending().take(topn).reduce { acc, i -> acc + i }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000.toBigInteger())
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(part1(input))
+
+    check(part2(testInput) == 45000.toBigInteger())
+    println(part2(input))
 }
