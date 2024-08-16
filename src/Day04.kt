bec4254e8e9635eb90adc27be77e6e77c93afb6b
Zero <zeroeeau@gmail.com>
2024-08-16 19:38:35 +0800

    Day04

---
+++ src/Day04.kt
@@ -0,0 +1,35 @@
+fun main() {
+    fun IntRange.contains(other: IntRange): Boolean {
+        return this.first <= other.first && this.last >= other.last
+    }
+
+    fun IntRange.overlaps(other: IntRange): Boolean {
+        return this.first <= other.last && other.first <= this.last
+    }
+
+    fun Boolean.toInt() = if (this) 1 else 0
+
+    fun processRanges(input: List<String>, check: (IntRange, IntRange) -> Int): Int = input.sumOf {
+        it.split(",").map { range ->
+            val (start, end) = range.split("-").map(String::toInt)
+            start..end
+        }.let { (first, second) -> check(first, second) }
+    }
+
+    fun part1(input: List<String>): Int = processRanges(input) {
+        first, second -> (first.contains(second) || second.contains(first)).toInt()
+    }
+
+    fun part2(input: List<String>): Int = processRanges(input) {
+        first, second -> (first.overlaps(second) || second.overlaps(first)).toInt()
+    }
+
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 2)
+
+    val input = readInput("Day04")
+    println(part1(input))
+
+    check(part2(testInput) == 4)
+    println(part2(input))
+}
---
+++ src/tpGen.kt
@@ -0,0 +1,25 @@
+import java.io.File
+
+fun createDayFile(templateFile: File, dayNumber: String) {
+    val dayFile = File("src/Day$dayNumber.kt")
+    if (dayFile.exists()) {
+        throw IllegalArgumentException("Day $dayNumber already exists.")
+    }
+    val pattern = Regex("""Day\d+""")
+    val content = templateFile.readText().replace(pattern, "Day$dayNumber")
+    dayFile.writeText(content)
+    println("Day $dayNumber created.")
+}
+
+fun createInputFiles(dayNumber: String) {
+    File("src/Day$dayNumber.txt").createNewFile()
+    File("src/Day${dayNumber}_test.txt").createNewFile()
+}
+
+fun main() {
+    val templateFile = File("src/template.kt")
+    print("Enter the day number: ")
+    val dayNumber = readLine()!!.toInt().toString().padStart(2, '0')
+    createDayFile(templateFile, dayNumber)
+    createInputFiles(dayNumber)
+}
