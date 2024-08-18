c5d965c03929eedf3f2eeb4f89e0ada14e7a7b31
Zero <zeroeeau@gmail.com>
2024-08-18 13:13:25 +0800

    Day06

--- src/template.kt
+++ src/template.kt
@@ -1,18 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Boolean {
-        return TODO("Implement part 1")
+    fun part1(input: List<String>): Int {
+        TODO("Implement part 1")
     }
 
-    fun part2(input: List<String>): Boolean {
-        return TODO("Implement part 2")
+    fun part2(input: List<String>): Int {
+        TODO("Implement part 2")
     }
 
     val testInput = readInput("Day01_test")
-    check(part1(testInput))
+    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
 
-    check(part2(testInput))
+    check(part2(testInput) == 1)
     println(part2(input))
 }
